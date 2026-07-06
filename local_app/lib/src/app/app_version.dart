9a1e34d90bbaa433b25393f63b42f6c76721edb0
mshzy <mshzy@users.noreply.github.com>
2026-07-06 16:30:56 +0800

    chore: bump to v1.0.9 - auto update check + error page filter

--- local_app/lib/src/app/app_version.dart
+++ local_app/lib/src/app/app_version.dart
@@ -1,8 +1,8 @@
 class AppVersion {
   const AppVersion._();
 
-  static const name = '1.0.8';
-  static const code = 28;
+  static const name = '1.0.9';
+  static const code = 29;
 
   static String get display => '$name ($code)';
 }
